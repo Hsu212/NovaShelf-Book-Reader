4648cff115a1abf9cc75d032bae75335a2aefea0
Hsu Myat Wai Maung <146718691+Hsu212@users.noreply.github.com>
2025-09-25 23:17:30 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.dbooks.org',
+        port: '',
+        pathname: '/img/books/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
